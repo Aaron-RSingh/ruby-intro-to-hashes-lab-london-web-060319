0726751dc9a501c35f8b67ce0b662a4ff60de476
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-30 10:08:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,7 +67,7 @@ monopoly = {
     }, 
     :names => {
       :reading_railroad => {"mortgage_value"=>"$100"}
-      },
+      ,
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline => {}
